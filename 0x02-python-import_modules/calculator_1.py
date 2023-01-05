c7fbab3656e43f848415a551374c6c748ae2cfba
Biyahon <biyahon@gmail.com>
2023-01-05 02:41:14 -0800

    task3

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,23 +1,43 @@
 #!/usr/bin/python3
+
 def add(a, b):
     """My addition function
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a + b
+
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
 
     Args:
     a: first integer
     b: second integer
 
     Returns:
-    The return value a + b
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a * b
 
     """
-
-    return (a + b)
-
-def sub(a, b):
-    return (a - b)
-
-def mul(a, b):
     return (a * b)
 
 def div(a, b):
+    """My division function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a / b
+
+    """
     return int(a / b)
