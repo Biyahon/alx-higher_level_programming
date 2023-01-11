507e16ba3c694cd102f011c2ce63a4c09fdae1ae
Biyahon <biyahon@gmail.com>
2023-01-11 05:12:46 -0800

    task2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    num = 0
+    for i in uniq_list:
+        num += i
+        return (num)
