f7638c1047ceffe8d7e89bdaba8882fb4718d752
Biyahon <biyahon@gmail.com>
2023-01-24 05:04:49 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" Creating a square class """
 
+""" Write a class Square that defines a square by: (based on 1-square.py)."""
 
 class Square:
-    """ Defining a class square """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Initializing a square class
-        Args: size=0: size of the square
+        """Initialize a new Square.
+        Args:
+        size (integer): The size of the new square.
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
         def area(self):
-            """ Calculating the area of the square """
-            return self.__size ** 2
+            """Returns the current square area"""
+            return (self.__size * self.__size)
